22103df41e72293820c959cedcb736b036ebf050
Jacó Apolinário <jacoapollinario@gmail.com>
2022-01-21 11:03:48 -0300

    news features

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import { Connection, createConnection } from 'typeorm';
+import { app } from '../../../../app';
+
+let connection: Connection
+describe('Create User Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection()
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  })
+
+  it('Should be able to create a new user', async () => {
+    const response = await request(app).post('/api/v1/users').send({
+      name: 'Teste User',
+      email: 'teste@teste.com.br',
+      password: '12345'
+    });
+
+    expect(response.status).toBe(201)
+  })
+})
